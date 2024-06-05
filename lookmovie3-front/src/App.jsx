8fd8d43e550a93a9780742216ed02c5c2cc1fab2
Ognjen Radovanovic <ognjen34car@gmail.com>
2024-06-05 15:50:11 +0200

    added for you page

--- lookmovie3-front/src/App.jsx
+++ lookmovie3-front/src/App.jsx
@@ -6,6 +6,9 @@ import { Container } from "@mui/material";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import MoviesPage from "./Pages/MoviesPage";
+import ForYouPage from "./Pages/ForYouPage";
+
 
 export const API_BASE_URL = "http://localhost:8080/api";
 
@@ -14,11 +17,20 @@ function App() {
     <ThemeProvider theme={themeOptions}>
       <Router>
         <Routes>
-          <Route path="/*" element={<HomePage />} />
+          <Route path="/*" element={<HomePage />} >
+
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="for-you" element={<ForYouPage />} />
+
+
+
+          </Route>
           <Route path="/login" element={<LoginComponent />} />
         </Routes>
 
-        <Container sx={{ height: "100%" }}></Container>
+        <Container sx={{ height: "100%" }}>
+
+        </Container>
       </Router>
     </ThemeProvider>
   );
--- lookmovie3-front/src/Components/NavbarComponent.jsx
+++ lookmovie3-front/src/Components/NavbarComponent.jsx
@@ -2,50 +2,65 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import BasicButton from "./BasicButton";
 import axios from "axios";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import { API_BASE_URL } from "../App";
-
-
 
 const NavbarComponent = ({ loggedUser }) => {
   const navigate = useNavigate();
   const handleLogout = async () => {
-    axios.post(`${API_BASE_URL}/auth/logout`, {}, { withCredentials: true })
-    .then((response) => {
-      console.log(response);
-      navigate("/login");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    axios
+      .post(`${API_BASE_URL}/auth/logout`, {}, { withCredentials: true })
+      .then((response) => {
+        console.log(response);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   const goHome = () => {
     navigate("/");
-  }
+  };
 
   return (
-    <Box sx={{ flexGrow: 1, margin :0 }}>
+    <Box sx={{ flexGrow: 1, margin: 0 }}>
       <AppBar position="static">
         <Toolbar>
-          
-          <Typography  onClick = {goHome} variant="h5" component="div" sx={{cursor: "pointer" }}>
+          <Typography
+            onClick={goHome}
+            variant="h5"
+            component="div"
+            sx={{ cursor: "pointer" }}
+          >
             Lookmovie3
           </Typography>
-          <Button color="inherit" sx={{marginLeft:"10px"}} onClick={handleLogout}>Home</Button>
-          <Button color="inherit" sx={{marginLeft:"10px"}} onClick={handleLogout}>Profile</Button>
+          <Link to={"movies"} sx ={{color:'white'}}>
+            <Button color="inherit" sx={{ marginLeft: "10px" }}>
+              Home
+            </Button>
+          </Link>
+          <Link to={"for-you"}>
+            <Button color="inherit" sx={{ marginLeft: "10px" }}>
+              For You
+            </Button>
+          </Link>
 
-
-          <Button color="inherit" sx = {{ position : "absolute",  marginRight:"5px",right :0}} onClick={handleLogout}>Logout</Button>
+          <Button
+            color="inherit"
+            sx={{ position: "absolute", marginRight: "5px", right: 0 }}
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
   );
-   
 };
 
 export default NavbarComponent;
